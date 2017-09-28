2b2c2055067cb2d9e532980e9baebcf018d5f463
Nikita Vintonovich <nikitavintonovich@MacBook-Pro-Nikita.local>
2017-09-28 23:55:50 +0300

    Added project from lecture

---
+++ Less_14/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Less_14
+//
+//  Created by Nikita Vintonovich on 9/26/17.
+//  Copyright © 2017 Vint-Rock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Less_14/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Less_14
+//
+//  Created by Nikita Vintonovich on 9/26/17.
+//  Copyright © 2017 Vint-Rock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
